96a35b4d851a03ff8ac8933a91005264da72888c
Danielle Jing <daniellejing@Danielles-MacBook-Pro.local>
2023-03-28 15:35:03 -0700

    new stuff

--- scripts.js
+++ scripts.js
@@ -17,73 +17,107 @@ const mapping = {
     },
     "KeyT": {
         sound: "p_open_hihat.wav", 
-        image: "wink.svg"
+        image: "heart1.svg"
     },
     "KeyY": {
         sound: "p_lofi_snare.wav", 
-        image: "wink.svg"
+        image: "heart2.svg"
     },
     "KeyU": {
         sound: "p_lofi_snap.wav", 
-        image: "wink.svg"
+        image: "heart3.svg"
     },
     "KeyI": {
         sound: "noise_chirp.wav", 
-        image: "wink.svg"
+        image: "heart4.svg"
     },
     "KeyO": {
         sound: "noise_crackle.wav", 
-        image: "wink.svg"
+        image: "heart5.svg"
     },
     "KeyP": {
         sound: "noise_rain.wav", 
-        image: "wink.svg"
+        image: "heart6.svg"
     },
     "KeyZ": {
         sound: "m_bordeaux.wav", 
-        image: "wink.svg"
+        image: "heart7.svg"
     },
     "KeyX": {
         sound: "m_deluxe_synth.wav", 
-        image: "wink.svg"
+        image: "heart8.svg"
     },
     "KeyC": {
         sound: "m_etern_piano.wav", 
-        image: "wink.svg"
+        image: "heart9.svg"
     },
     "KeyV": {
         sound: "m_etern_piano2.wav", 
-        image: "wink.svg"
+        image: "heart10.svg"
     },
     "KeyB": {
         sound: "m_exp_1.wav", 
-        image: "wink.svg"
+        image: "heart11.svg"
     },
     "KeyN": {
         sound: "m_exp_2.wav", 
-        image: "wink.svg"
+        image: "heart12.svg"
     },
     "KeyM": {
         sound: "m_mountains.wav", 
-        image: "wink.svg"
+        image: "heart13.svg"
     },
     "KeyH": {
         sound: "os_cartoon.wav", 
-        image: "wink.svg"
+        image: "heart14.svg"
     },
     "KeyJ": {
         sound: "os_frozen.wav", 
-        image: "wink.svg"
+        image: "heart15.svg"
     },
     "KeyK": {
         sound: "os_kalimba.wav", 
-        image: "wink.svg"
+        image: "heart16.svg"
     },
     "KeyL": {
         sound: "os_subtle.wav", 
+        image: "heart17.svg"
+    },
+    "KeyA": {
+        sound: "g_deluxe_lofi.wav", 
+        image: "heart18.svg"
+    },
+    "KeyS": {
+        sound: "g_dreams.wav", 
+        image: "heart19.svg"
+    },
+    "KeyD": {
+        sound: "g_etern_elec.wav", 
+        image: "heart20.svg"
+    },
+    "KeyF": {
+        sound: "g_loop_1.wav", 
+        image: "heart1.svg"
+    },
+    "KeyG": {
+        sound: "g_loop_2.wav", 
         image: "wink.svg"
     },
 };
+
+const text = "This is my soundboard";
+let i = 0;
+
+function typeEffect() {
+  const div = document.getElementById("typing-effect");
+  if (i < text.length) {
+    div.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  }
+}
+
+typeEffect();
 
 var playable = new Audio()
 document.addEventListener("keydown", function(e) {
